9e23aa08d5230391301774a954910c0ddeaabfbe
uriel <uriel.garcia@memphis.mx>
2024-07-08 11:03:00 -0600

    Se agregan las migraciones correspondientes para la tabla temporal y el campo extra en users de uyserType

---
+++ database/migrations/2024_07_08_170010_add_temp_poblacion_tb.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('temp_poblacion', function (Blueprint $table) {
+            $table->string('nombre');
+            $table->string('paterno');
+            $table->string('materno');
+            $table->string('telefono');
+            $table->string('calle');
+            $table->string('numero_exterior');
+            $table->string('numero_interior')->nullable();
+            $table->string('colonia');
+            $table->string('cp');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('temp_poblacion');
+    }
+};
